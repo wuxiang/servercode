600022e1a1919f5e2cd120f80ef65136cd0f46f3
wuxiang <wuxiangfe@qq.com>
2012-02-29 01:21:54 -0800

    add more debug information to shift opeator

--- serverApi/shift_operator.cpp
+++ serverApi/shift_operator.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    uint32_t result = 0;
+    uint64_t result = 0;
     result = result | (1 & 0xff);
     std::cout << result << std::endl;
 
@@ -12,5 +12,15 @@ int main()
 
     result = result | (1 << 16) & 0xff0000;
     std::cout << result << std::endl;
+
+    std::cout << "===========================" << std::endl;
+    for(int i = 0; i < 7; ++i)
+    {
+        result = 1;
+        std::cout << ((result << (i * 8) ) & ((uint64_t)(0xff) << (i * 8) )) << std::endl;
+        //std::cout << ((result << (i * 8) ) & (0xff << (i * 8) )) << std::endl;
+    }
+
+    std::cout << (result << 0) << std::endl;
     return 0;
 }
